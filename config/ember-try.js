55e71f517e7f61ce9ef94e08b68ddb4328c2d7ff
Robert Wagner <rwwagner90@gmail.com>
2021-12-07 09:43:39 -0500

    Make htmlbars a devDep

--- config/ember-try.js
+++ config/ember-try.js
@@ -3,7 +3,7 @@
 
 const getChannelURL = require('ember-source-channel-url');
 
-module.exports = async function() {
+module.exports = async function () {
   return {
     useYarn: true,
     scenarios: [
@@ -11,7 +11,8 @@ module.exports = async function() {
         name: 'ember-lts-2.12',
         npm: {
           devDependencies: {
-            'ember-source': '~2.12.0'
+            'ember-source': '~2.12.0',
+            'ember-cli-htmlbars': '^4.3.1',
           },
         },
       },
@@ -19,7 +20,8 @@ module.exports = async function() {
         name: 'ember-lts-2.18',
         npm: {
           devDependencies: {
-            'ember-source': '~2.18.0'
+            'ember-source': '~2.18.0',
+            'ember-cli-htmlbars': '^4.3.1',
           },
         },
       },
@@ -27,73 +29,74 @@ module.exports = async function() {
         name: 'ember-lts-3.4',
         npm: {
           devDependencies: {
-            'ember-source': '~3.4.0'
-          }
-        }
+            'ember-source': '~3.4.0',
+            'ember-cli-htmlbars': '^4.3.1',
+          },
+        },
       },
       {
         name: 'ember-lts-3.8',
         npm: {
           devDependencies: {
-            'ember-source': '~3.8.0'
-          }
-        }
+            'ember-source': '~3.8.0',
+          },
+        },
       },
       {
         name: 'ember-lts-3.12',
         npm: {
           devDependencies: {
-            'ember-source': '~3.12.0'
-          }
-        }
+            'ember-source': '~3.12.0',
+          },
+        },
       },
       {
         name: 'ember-lts-3.16',
         npm: {
           devDependencies: {
-            'ember-source': '~3.16.0'
-          }
-        }
+            'ember-source': '~3.16.0',
+          },
+        },
       },
       {
         name: 'ember-lts-3.20',
         npm: {
           devDependencies: {
-            'ember-source': '~3.20.0'
-          }
-        }
+            'ember-source': '~3.20.0',
+          },
+        },
       },
       {
-      name: 'ember-lts-3.24',
+        name: 'ember-lts-3.24',
         npm: {
           devDependencies: {
             'ember-source': '~3.24.3',
-          }
+          },
         },
       },
       {
         name: 'ember-release',
         npm: {
           devDependencies: {
-            'ember-source': await getChannelURL('release')
-          }
-        }
+            'ember-source': await getChannelURL('release'),
+          },
+        },
       },
       {
         name: 'ember-beta',
         npm: {
           devDependencies: {
-            'ember-source': await getChannelURL('beta')
-          }
-        }
+            'ember-source': await getChannelURL('beta'),
+          },
+        },
       },
       {
         name: 'ember-canary',
         npm: {
           devDependencies: {
-            'ember-source': await getChannelURL('canary')
-          }
-        }
+            'ember-source': await getChannelURL('canary'),
+          },
+        },
       },
       // The default `.travis.yml` runs this scenario via `yarn test`,
       // not via `ember try`. It's still included here so that running
@@ -102,21 +105,21 @@ module.exports = async function() {
       {
         name: 'ember-default',
         npm: {
-          devDependencies: {}
-        }
+          devDependencies: {},
+        },
       },
       {
         name: 'ember-default-with-jquery',
         env: {
           EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'jquery-integration': true
-          })
+            'jquery-integration': true,
+          }),
         },
         npm: {
           devDependencies: {
-            '@ember/jquery': '^1.1.0'
-          }
-        }
+            '@ember/jquery': '^1.1.0',
+          },
+        },
       },
       {
         name: 'ember-classic',
@@ -133,6 +136,6 @@ module.exports = async function() {
           },
         },
       },
-    ]
+    ],
   };
-}
+};
