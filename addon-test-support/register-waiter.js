9fe4b8b701ffb4a1e5874d800e3f40864b3c2d1f
Robert Wagner <rwwagner90@gmail.com>
2021-12-07 11:44:28 -0500

    Update register-waiter.js

--- addon-test-support/register-waiter.js
+++ addon-test-support/register-waiter.js
@@ -1,8 +1,9 @@
+/* eslint-disable ember/no-legacy-test-waiters */
 import { registerWaiter as emberRegisterWaiter } from '@ember/test';
 import scheduler from 'ember-raf-scheduler';
 
 export default function registerWaiter() {
-  emberRegisterWaiter(function() {
+  emberRegisterWaiter(function () {
     return scheduler.jobs === 0;
   });
 }
