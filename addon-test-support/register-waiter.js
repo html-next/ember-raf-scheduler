3707db416a512bd0653f9f7106cbe15322883d46
Matthew Beale <matt.beale@madhatted.com>
2021-12-07 19:25:02 -0500

    Use an import instead of global

--- addon-test-support/register-waiter.js
+++ addon-test-support/register-waiter.js
@@ -1,10 +1,8 @@
 import scheduler from 'ember-raf-scheduler';
+import { registerWaiter } from '@ember/test';
 
 export default function registerWaiter() {
-  // We can't rely on the importable Ember since shims are no
-  // longer included by default, so use the global instance.
-  // eslint-disable-next-line
-  Ember.Test.registerWaiter(function() {
+  registerWaiter(function() {
     return scheduler.jobs === 0;
   });
 }
