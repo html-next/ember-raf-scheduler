23cd9715e0e5603168df70db83901d9767b3592f
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-05-16 17:23:47 -0400

    Add scenario testing

---
+++ .try.mjs
@@ -0,0 +1,69 @@
+export default scenarios();
+
+function scenarios() {
+  return {
+    scenarios: [
+      compatEmberScenario('ember-lts-3.28', '^3.28.0'),
+      compatEmberScenario('ember-lts-4.4', '~4.4.0'),
+      compatEmberScenario('ember-lts-4.12', '^4.12.0'),
+      compatEmberScenario('ember-lts-5.4', '~5.4.0'),
+      compatEmberScenario('ember-lts-5.12', '^5.12.0'),
+      compatEmberScenario('ember-lts-6.4', '~6.4.0'),
+      {
+        name: 'ember-latest',
+        npm: {
+          devDependencies: {
+            'ember-source': 'npm:ember-source@latest',
+          },
+        },
+      },
+      {
+        name: 'ember-beta',
+        npm: {
+          devDependencies: {
+            'ember-source': 'npm:ember-source@beta',
+          },
+        },
+      },
+      {
+        name: 'ember-canary',
+        npm: {
+          devDependencies: {
+            'ember-source': 'npm:ember-source@alpha',
+          },
+        },
+      },
+    ],
+  };
+}
+
+function emberCliBuildJS() {
+  return `const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const { compatBuild } = require('@embroider/compat');
+module.exports = async function (defaults) {
+  const { buildOnce } = await import('@embroider/vite');
+  let app = new EmberApp(defaults);
+  return compatBuild(app, buildOnce);
+};`;
+}
+
+function compatEmberScenario(name, emberVersion) {
+  return {
+    name,
+    npm: {
+      devDependencies: {
+        'ember-source': emberVersion,
+        '@embroider/compat': '^4.0.0-alpha.14',
+        'ember-cli': '^5.12.0',
+        'ember-auto-import': '^2.10.0',
+        '@ember/optional-features': '^2.2.0',
+      },
+    },
+    env: {
+      ENABLE_COMPAT_BUILD: true,
+    },
+    files: {
+      'ember-cli-build.js': emberCliBuildJS(),
+    },
+  };
+}
